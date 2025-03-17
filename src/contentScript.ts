f2f7d99ef18be52d5513a2520c1c562200f2e353
Steven Tohme <steventohme59@gmail.com>
2025-03-17 18:32:00 -0400

    create custom button next to send

--- src/contentScript.ts
+++ src/contentScript.ts
@@ -1,1 +1,34 @@
-console.log("content script loaded");
+function addCustomButtons() {
+	const sendButtons = document.querySelectorAll(
+		"button.msg-form__send-button.artdeco-button.artdeco-button--1",
+	);
+
+	for (const sendButton of Array.from(sendButtons)) {
+		if (
+			sendButton.parentElement &&
+			!sendButton.parentElement.querySelector(".my-custom-button")
+		) {
+			const customButton = document.createElement("button");
+			customButton.className = "my-custom-button";
+			customButton.textContent = "Custom Action";
+			customButton.style.marginLeft = "8px";
+
+			customButton.addEventListener("click", () => {
+				console.log("Custom button clicked!");
+			});
+
+			sendButton.parentElement.insertBefore(
+				customButton,
+				sendButton.nextSibling,
+			);
+		}
+	}
+}
+
+const observer = new MutationObserver(() => {
+	addCustomButtons();
+});
+
+observer.observe(document.body, { childList: true, subtree: true });
+
+addCustomButtons();
